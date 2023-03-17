a7c848ff881d42d5a3f054cd3bb2610ef8874408
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-03-18 01:32:33 +0400

    style back-button component

--- src/components/back-button/back-button.tsx
+++ src/components/back-button/back-button.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import { BackArrowIcon } from './assets/back-arrow-icon'
 import styles from './styles/back-button.module.scss'
 
 export const BackButton = () => {
+	const router = useRouter()
+
 	return (
-		<button className={styles.container}>
+		<button onClick={() => router.push(`/`)} className={styles.container}>
 			<BackArrowIcon />
 		</button>
 	)
