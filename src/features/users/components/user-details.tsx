4af09c0c00f625425f7eff3ff4e3575364e8a788
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-03-18 01:33:40 +0400

    add header to user page

--- src/features/users/components/user-details.tsx
+++ src/features/users/components/user-details.tsx
@@ -9,6 +9,7 @@ import { UserFriends } from './user-friends'
 import { UserHistory } from './user-history'
 import styles from './styles/user-details.module.scss'
 import { useUser } from '@/features/users/hooks'
+import { BackButton } from '@/components/back-button'
 
 export const UserDetails = () => {
 	const pathname = usePathname()
@@ -16,46 +17,49 @@ export const UserDetails = () => {
 
 	const { user, loading, isError, error } = useUser(userId)
 
-	if (loading) return <LoadingSpinner />
+	if (loading || !user) return <LoadingSpinner />
 
 	if (isError) return <div>{error}</div>
 
-	if (user)
-		return (
-			<div>
-				<div className={styles.userDetails}>
-					<div className={styles.avatar}>
-						<Image
-							src={user?.imageUrl}
-							alt='user avatar'
-							width={500}
-							height={500}
-						/>
-					</div>
-
-					<div className={styles.info}>
-						<UserInfo
-							fullName={`${user?.prefix} ${user?.name} ${user?.lastName}`}
-							position={user?.title}
-							email={user?.email}
-							ipAddress={user?.ip}
-							jobArea={user?.jobArea}
-							jobType={user?.jobType}
-						/>
-					</div>
-
-					<div className={styles.address}>
-						<UserAddress address={user?.address} />
-					</div>
+	return (
+		<div className={styles.container}>
+			<header>
+				<BackButton />
+				<h1>{`${user?.prefix} ${user?.name} ${user?.lastName}`}</h1>
+			</header>
+			<div className={styles.userDetails}>
+				<div className={styles.avatar}>
+					<Image
+						src={user?.imageUrl}
+						alt='user avatar'
+						width={500}
+						height={500}
+					/>
 				</div>
 
-				<div className={styles.history}>
-					<UserHistory />
+				<div className={styles.info}>
+					<UserInfo
+						fullName={`${user?.prefix} ${user?.name} ${user?.lastName}`}
+						position={user?.title}
+						email={user?.email}
+						ipAddress={user?.ip}
+						jobArea={user?.jobArea}
+						jobType={user?.jobType}
+					/>
 				</div>
 
-				<div className={styles.friends}>
-					<UserFriends userId={userId} />
+				<div className={styles.address}>
+					<UserAddress address={user?.address} />
 				</div>
 			</div>
-		)
+
+			<div className={styles.history}>
+				<UserHistory />
+			</div>
+
+			<div className={styles.friends}>
+				<UserFriends userId={userId} />
+			</div>
+		</div>
+	)
 }
